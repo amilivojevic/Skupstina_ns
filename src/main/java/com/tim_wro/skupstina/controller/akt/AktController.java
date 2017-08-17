7506cd192d482154672217dd4aa21f52cd377f6b
Aleksandra Milivojevic <aleksandra.milivojevic.94@gmail.com>
2017-08-17 20:17:54 +0200

    dodavanje akta frontend: dodavanje tacke

--- src/main/java/com/tim_wro/skupstina/controller/akt/AktController.java
+++ src/main/java/com/tim_wro/skupstina/controller/akt/AktController.java
@@ -1,6 +1,5 @@
 package com.tim_wro.skupstina.controller.akt;
 
-import com.tim_wro.skupstina.model.Akt;
 import com.tim_wro.skupstina.model.Korisnik;
 import com.tim_wro.skupstina.services.AktService;
 import com.tim_wro.skupstina.util.ResponseMessage;
@@ -12,8 +11,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
-
 
 /**
  * Created by Nina on 16-Jun-17.
@@ -21,9 +18,6 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/acts")
-/** Sta je to nama**/
-
-
 public class AktController {
 
     private final AktService aktService;
--- src/main/webapp/app/act/addAct.controller.js
+++ src/main/webapp/app/act/addAct.controller.js
@@ -5,9 +5,17 @@
     angular.module("skupstinaNS")
         .controller("AddActController", addActController);
 
-    function addActController(AddActService) {
+    function addActController(AddActService, $scope) {
         var vm = this;
         vm.addAct = addAct;
+        vm.dodajDeoNaFront = dodajDeoNaFront;
+        vm.dodajClanNaFront = dodajClanNaFront;
+        vm.dodajSadrzaj_clan = dodajSadrzaj_clan;
+        vm.dodajStav_clan = dodajStav_clan;
+        vm.dodajTacku_stav_clan = dodajTacku_stav_clan;
+        vm.dodajSadrzaj_stav_clan = dodajSadrzaj_stav_clan;
+
+
 
         function addAct() {
             vm.new_act = {
@@ -22,13 +30,93 @@
             console.log("novi akt: " + JSON.stringify(vm.newAct))
 
 
-            AddActService.addAct(vm.new_act).then(function (response) {
+           /* AddActService.addAct(vm.new_act).then(function (response) {
                 console.log("valjda je kreiran: " + response.data.response)
             }, function (response) {
                 alert(response.data);
-            });
+            });*/
 
         }
+
+
+
+
+        $scope.delovi=[];
+        $scope.clanovi=[];
+        $scope.sadrzaj = [];
+        vm.disableDodajDeo = false;
+        vm.disableDodajClan = false;
+
+//*****************************************************************
+
+        function dodajDeoNaFront(){
+            console.log("usaо u dodajDeoNaFront");
+            $scope.naslov1 = "Dodavanje Dela";
+            vm.disableDodajClan = true;
+
+        }
+
+        function dodajClanNaFront(){
+            console.log("usaо u dodajClanNaFront");
+            $scope.naslov1 = "Dodavanje Clana";
+            vm.disableDodajDeo = true;
+            $scope.clanovi.push(
+                {
+                    "atributi":{
+                        "naziv" : '',
+                        "br" : '',
+                    },
+                    "disableDodajStav" : false,
+                    "disableDodajSadrzaj" : false,
+                    "stavovi" : [],
+                    "sadrzaj" : []
+                }
+            );
+        }
+
+        function dodajSadrzaj_clan(index){
+            console.log("usaо u dodajSadrzajNaFront");
+
+            $scope.clanovi[index].disableDodajStav = true;
+            $scope.clanovi[index].disableDodajSadrzaj = true;
+            $scope.clanovi[index].sadrzaj.push("");
+        }
+
+        function dodajStav_clan(index){
+            console.log("usaо u dodajStavNaFront");
+            $scope.clanovi[index].disableDodajSadrzaj = true;
+            $scope.clanovi[index].stavovi.push(
+                {
+                    "disableDodajSadrzaj": false,
+                    "disableDodajTacku":false,
+                    "tacke" : [],
+                    "sadrzaj" : []
+                }
+            )
+        }
+
+
+        function dodajTacku_stav_clan(index_clana,index_stava){
+            console.log("usaо u dodajTacku_stav_clan");
+            $scope.clanovi[index_clana].stavovi[index_stava].disableDodajSadrzaj = true;
+            $scope.clanovi[index_clana].stavovi[index_stava].tacke.push(
+                {
+                    "atributi" : { "br" : ""},
+                    "podtacke" : [],
+                    "sadrzaj" : []
+                }
+            )
+        }
+
+        function dodajSadrzaj_stav_clan(index_clana,index_stava){
+            console.log("usaо u dodajSadrzaj_stav_clan");
+
+            $scope.clanovi[index_clana].stavovi[index_stava].disableDodajSadrzaj = true;
+            $scope.clanovi[index_clana].stavovi[index_stava].disableDodajTacku = true;
+            $scope.clanovi[index_clana].stavovi[index_stava].sadrzaj.push("");
+        }
+
+//*****************************************************************
 
     }
 })();
