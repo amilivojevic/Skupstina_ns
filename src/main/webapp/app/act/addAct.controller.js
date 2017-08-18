11c8220480d386880e016568c33151d16932cd64
Aleksandra Milivojevic <aleksandra.milivojevic.94@gmail.com>
2017-08-18 03:36:42 +0200

    radi kreiranje novog akta na frontu

--- src/main/webapp/app/act/addAct.controller.js
+++ src/main/webapp/app/act/addAct.controller.js
@@ -14,6 +14,10 @@
         vm.dodajStav_clan = dodajStav_clan;
         vm.dodajTacku_stav_clan = dodajTacku_stav_clan;
         vm.dodajSadrzaj_stav_clan = dodajSadrzaj_stav_clan;
+        vm.dodajPodtacku_tacka_stav_clan = dodajPodtacku_tacka_stav_clan;
+        vm.dodajSadrzaj_tacka_stav_clan = dodajSadrzaj_tacka_stav_clan;
+        vm.dodajAlineju_podtacku_tacka_stav_clan = dodajAlineju_podtacku_tacka_stav_clan;
+        vm.dodajSadrzaj_podtacka_tacka_stav_clan = dodajSadrzaj_podtacka_tacka_stav_clan;
 
 
 
@@ -27,14 +31,13 @@
                 stanje: vm.newAct.stanje
             }
 
-            console.log("novi akt: " + JSON.stringify(vm.newAct))
+            console.log("novi akt: " + JSON.stringify(vm.newAct));
+            console.log("kreirani clanovi: " + JSON.stringify($scope.clanovi));
+            console.log("transformisani clanovi: " + JSON.stringify(transformClanovi($scope)));
 
-
-           /* AddActService.addAct(vm.new_act).then(function (response) {
-                console.log("valjda je kreiran: " + response.data.response)
-            }, function (response) {
-                alert(response.data);
-            });*/
+            var clanoviREST = {
+                "clanovi" : transformClanovi($scope)
+            }
 
         }
 
@@ -104,6 +107,11 @@
                     "atributi" : { "br" : ""},
                     "podtacke" : [],
                     "sadrzaj" : []
+                },
+                {
+                    "atributi" : { "br" : ""},
+                    "podtacke" : [],
+                    "sadrzaj" : []
                 }
             )
         }
@@ -115,8 +123,103 @@
             $scope.clanovi[index_clana].stavovi[index_stava].disableDodajTacku = true;
             $scope.clanovi[index_clana].stavovi[index_stava].sadrzaj.push("");
         }
+        //dodajPodtacku_tacka_stav_clan
+
+        function dodajPodtacku_tacka_stav_clan(index_clana,index_stava,index_tacke){
+            console.log("usaо u dodajPodtacku_tacka_stav_clan");
+            $scope.clanovi[index_clana].stavovi[index_stava].tacke[index_tacke].disableDodajSadrzaj = true;
+            $scope.clanovi[index_clana].stavovi[index_stava].tacke[index_tacke].podtacke.push(
+                {
+                    "atributi" : { "br" : ""},
+                    "alineje" : [],
+                    "sadrzaj" : []
+                },
+                {
+                    "atributi" : { "br" : ""},
+                    "alineje" : [],
+                    "sadrzaj" : []
+                }
+            )
+        }
+
+        function dodajSadrzaj_tacka_stav_clan(index_clana,index_stava,index_tacke){
+            console.log("usaо u dodajSadrzaj_tacka_stav_clan");
+
+            $scope.clanovi[index_clana].stavovi[index_stava].tacke[index_tacke].disableDodajSadrzaj = true;
+            $scope.clanovi[index_clana].stavovi[index_stava].tacke[index_tacke].disableDodajPodtacku = true;
+            $scope.clanovi[index_clana].stavovi[index_stava].tacke[index_tacke].sadrzaj.push("");
+        }
+
+        function dodajAlineju_podtacku_tacka_stav_clan(index_clana,index_stava,index_tacke,index_podtacke){
+            console.log("usaо u dodajAlineju_podtacku_tacka_stav_clan");
+            $scope.clanovi[index_clana].stavovi[index_stava].tacke[index_tacke].podtacke[index_podtacke].disableDodajSadrzaj = true;
+            $scope.clanovi[index_clana].stavovi[index_stava].tacke[index_tacke].podtacke[index_podtacke].disableDodajAlineju = true;
+            $scope.clanovi[index_clana].stavovi[index_stava].tacke[index_tacke].podtacke[index_podtacke].alineje.push(
+                {
+                    "sadrzaj" : []
+                }
+            )
+        }
+
+        function dodajSadrzaj_podtacka_tacka_stav_clan(index_clana,index_stava,index_tacke,index_podtacke){
+            console.log("usaо u dodajSadrzaj_podtacka_tacka_stav_clan");
+
+            $scope.clanovi[index_clana].stavovi[index_stava].tacke[index_tacke].podtacke[index_podtacke].disableDodajSadrzaj = true;
+            $scope.clanovi[index_clana].stavovi[index_stava].tacke[index_tacke].podtacke[index_podtacke].disableDodajAlineju = true;
+            $scope.clanovi[index_clana].stavovi[index_stava].tacke[index_tacke].podtacke[index_podtacke].sadrzaj.push("");
+        }
 
 //*****************************************************************
 
+    }
+
+    function transformClanovi($scope){
+        var clanoviNovi = [];
+        var clan;
+        for(var i=0; i<$scope.clanovi.length; i++){
+            clan = {};
+
+            clan.atributi = $scope.clanovi[i].atributi;
+            if($scope.clanovi[i].stavovi.length > 0){
+                clan.stavovi = [];
+                for(var j=0; j<$scope.clanovi[i].stavovi.length; j++) {
+                    clan.stavovi.push({});
+                    if ($scope.clanovi[i].stavovi[j].tacke.length > 0) {
+                        clan.stavovi[j].tacke = [];
+                        for(var k=0; k<$scope.clanovi[i].stavovi[j].tacke.length; k++) {
+                            clan.stavovi[j].tacke.push({});
+                            clan.stavovi[j].tacke[k].atributi = $scope.clanovi[i].stavovi[j].tacke[k].atributi;
+                            if ($scope.clanovi[i].stavovi[j].tacke[k].podtacke.length > 0) {
+                                clan.stavovi[j].tacke[k].podtacke = [];
+                                for(var l=0; l<$scope.clanovi[i].stavovi[j].tacke[k].podtacke.length; l++) {
+                                    clan.stavovi[j].tacke[k].podtacke.push({});
+                                    clan.stavovi[j].tacke[k].podtacke[l].atributi = $scope.clanovi[i].stavovi[j].tacke[k].podtacke[l].atributi;
+
+                                    if ($scope.clanovi[i].stavovi[j].tacke[k].podtacke[l].alineje.length > 0) {
+                                        clan.stavovi[j].tacke[k].podtacke[l].alineje = $scope.clanovi[i].stavovi[j].tacke[k].podtacke[l].alineje;
+                                    }
+
+                                    if ($scope.clanovi[i].stavovi[j].tacke[k].podtacke[l].sadrzaj.length > 0) {
+                                        clan.stavovi[j].tacke[k].podtacke[l].sadrzaj = $scope.clanovi[i].stavovi[j].tacke[k].podtacke[l].sadrzaj[0];
+                                    }
+                                }
+                            }
+                            if ($scope.clanovi[i].stavovi[j].tacke[k].sadrzaj.length > 0) {
+                                clan.stavovi[j].tacke[k].sadrzaj = $scope.clanovi[i].stavovi[j].tacke[k].sadrzaj[0];
+                            }
+                        }
+                    }
+                    if ($scope.clanovi[i].stavovi[j].sadrzaj.length > 0) {
+
+                        clan.stavovi[j].sadrzaj = $scope.clanovi[i].stavovi[j].sadrzaj[0];
+                    }
+                }
+            }
+            if($scope.clanovi[i].sadrzaj.length > 0){
+                clan.sadrzaj = $scope.clanovi[i].sadrzaj[0];
+            }
+            clanoviNovi.push(clan);
+        }
+        return clanoviNovi;
     }
 })();
